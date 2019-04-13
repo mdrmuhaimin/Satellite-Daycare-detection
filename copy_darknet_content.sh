e9cc1edfb0014b4ad6545bab8fab2b3a91685ffc
Muhammad Muhaimin <muhammad.r.muhaimin@gmail.com>
2019-04-13 03:15:24 -0700

    Add tester file

--- copy_darknet_content.sh
+++ copy_darknet_content.sh
@@ -3,6 +3,8 @@ cp -r Darknet_content/data/* darknet/data/
 cp Darknet_content/get_weight.sh darknet/
 cp Darknet_content/train.sh darknet/
 cp Darknet_content/test_run.sh darknet/
+cp Darknet_content/darknet.py darknet/
+cp Darknet_content/Darknet.ipynb darknet/
 ls darknet/cfg | grep daycare
 ls darknet/data | grep daycare
 ls darknet | grep .sh
